59f917ca679ccfbc632d986207e8dd2cbbc86bc7
yura <yura11011@yandex.com>
2022-04-14 18:40:52 +0300

    cookieDB

---
+++ internal/AuthDb/AuthDb.cpp
@@ -0,0 +1,13 @@
+//
+// Created by yura11011 on 14.04.2022.
+//
+
+#include "AuthDb.h"
+
+std::string AuthDb::GetCookie(std::string Id) {
+    return std::string();
+}
+
+std::string AuthDb::SetCookie(std::string Id) {
+    return std::string();
+}
