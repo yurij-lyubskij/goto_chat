3c18cfc5e31c4bf138d57f10696aa20f71ec8746
FackOff25 <kozvladimir@bk.ru>
2022-04-15 21:23:22 +0300

    Added MessageQueue to WebSocketServer

---
+++ internal/MessageQueue/MessageQueue.h
@@ -0,0 +1,26 @@
+#ifndef GOTO_CHAT_MESSAGEQUEUE_H
+#define GOTO_CHAT_MESSAGEQUEUE_H
+
+#include <queue>
+#include <memory>
+
+#include "Message.h"
+
+class iMessageQueue {
+public:
+    virtual ~iMessageQueue() = default;
+    virtual void Push(std::shared_ptr<iMessage>) = 0;
+
+    virtual std::shared_ptr<iMessage> Pop() = 0;
+};
+
+class MessageQueue :public iMessageQueue{
+    std::queue<std::shared_ptr<iMessage>> messages;
+public:
+    void Push(std::shared_ptr<iMessage>) override;
+
+    std::shared_ptr<iMessage> Pop() override;
+};
+
+
+#endif //GOTO_CHAT_MESSAGEQUEUE_H
