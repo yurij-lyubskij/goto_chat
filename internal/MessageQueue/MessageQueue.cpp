2419641f966b12e3a3ab8fb1e53eabadd6d35ecd
FackOff25 <kozvladimir@bk.ru>
2022-05-11 11:39:49 +0300

    MessageQueue implementation

--- internal/MessageQueue/MessageQueue.cpp
+++ internal/MessageQueue/MessageQueue.cpp
@@ -1,1 +1,11 @@
 #include "MessageQueue.h"
+
+void MessageQueue::Push(std::shared_ptr<iMessage> mes){
+	messages.push(mes);
+};
+
+std::shared_ptr<iMessage> MessageQueue::Pop(){
+	std::shared_ptr<iMessage> message = messages.front();
+	messages.pop();
+	return message;
+};
