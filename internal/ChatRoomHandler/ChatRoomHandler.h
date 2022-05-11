6b3e07a44a8d361738059780a09b358dd3fc00b5
FackOff25 <kozvladimir@bk.ru>
2022-05-11 11:40:57 +0300

    ChatRoomHandler override

--- internal/ChatRoomHandler/ChatRoomHandler.h
+++ internal/ChatRoomHandler/ChatRoomHandler.h
@@ -7,32 +7,32 @@
 
 class GetMessageFromChat: public iHandler{
 	public:
-		bool canHandle(Request);
-		Response handle(Request);
+		bool canHandle(Request) override;
+		Response handle(Request) override;
 };
 
 class CreateChatRoom: public iHandler{
 	public:
-		bool canHandle(Request);
-		Response handle(Request);
+		bool canHandle(Request) override;
+		Response handle(Request) override;
 };
 
 class JoinChatRoom: public iHandler{
 	public:
-		bool canHandle(Request);
-		Response handle(Request);
+		bool canHandle(Request) override;
+		Response handle(Request) override;
 };
 
 class FindChatRoom: public iHandler{
 	public:
-		bool canHandle(Request);
-		Response handle(Request);
+		bool canHandle(Request) override;
+		Response handle(Request) override;
 };
 
 class NotifyUsers: public iHandler{
 	public:
-		bool canHandle(Request);
-		Response handle(Request);
+		bool canHandle(Request) override;
+		Response handle(Request) override;
 };
 
 #endif
