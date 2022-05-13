4dbd2816fa6082d1ed1849211c7ed9129dedf57e
FackOff25 <kozvladimir@bk.ru>
2022-05-13 19:49:18 +0000

    VoiceMessage basic implementation

--- internal/Message/Message.cpp
+++ internal/Message/Message.cpp
@@ -1,10 +1,15 @@
 #include "Message.h"
 
+//
+//(Text)Message section
+//
+
 Message::Message(){ 
 	id = -1;
 	content = "";
 	sendTime = 0;
 	senderId = 0;
+	type = text;
 };
 
 Message::Message(int _id) { 
@@ -12,6 +17,7 @@ Message::Message(int _id) {
 	content = "";
 	sendTime = 0;
 	senderId = 0;
+	type = text;
 };
 
 Message::Message(std::string _content, time_t time, int sender){
@@ -19,6 +25,7 @@ Message::Message(std::string _content, time_t time, int sender){
 	content = _content;
 	sendTime = time;
 	senderId = sender;
+	type = text;
 };
 
 Message::Message(int _id, std::string _content, time_t time, int sender){
@@ -26,6 +33,7 @@ Message::Message(int _id, std::string _content, time_t time, int sender){
 	content = _content;
 	sendTime = time;
 	senderId = sender;
+	type = text;
 };
 
 Message::Message(const Message& mes){
@@ -33,8 +41,65 @@ Message::Message(const Message& mes){
 	senderId = mes.senderId;
 	sendTime = mes.sendTime;
 	content = mes.content;
+	type = text;
 };
 
 Message Message::operator=(const Message& mes){
 	return Message(mes);
 }
+
+//
+//end of (Text)Message section
+//
+
+
+//
+//VoiceMessage section
+//
+VoiceMessage::VoiceMessage(){ 
+	id = -1;
+	content = "";
+	sendTime = 0;
+	senderId = 0;
+	type = voice;
+};
+
+VoiceMessage::VoiceMessage(int _id) { 
+	id = _id;
+	content = "";
+	sendTime = 0;
+	senderId = 0;
+	type = voice;
+};
+
+VoiceMessage::VoiceMessage(std::string _content, time_t time, int sender){
+	id = 0;
+	content = _content;
+	sendTime = time;
+	senderId = sender;
+	type = voice;
+};
+
+VoiceMessage::VoiceMessage(int _id, std::string _content, time_t time, int sender){
+	id = _id;
+	content = _content;
+	sendTime = time;
+	senderId = sender;
+	type = voice;
+};
+
+VoiceMessage::VoiceMessage(const VoiceMessage& mes){
+	id = mes.id;
+	senderId = mes.senderId;
+	sendTime = mes.sendTime;
+	content = mes.content;
+	type = voice;
+};
+
+VoiceMessage VoiceMessage::operator=(const VoiceMessage& mes){
+	return VoiceMessage(mes);
+}
+
+//
+//end of VoiceMessage section
+//
--- internal/Message/Message.h
+++ internal/Message/Message.h
@@ -6,12 +6,15 @@
 
 #include "User.h"
 
+enum MessegeType{ text, voice };
+
 class iMessage{
 	protected:
 		int id;
 		time_t sendTime;
 		std::string content;
 		int senderId;
+		enum MessegeType type;
 	public:
 		int getId() const { return id; };
 		int getSender() const { return senderId; };
@@ -20,6 +23,7 @@ class iMessage{
 };
 
 class Message: public iMessage{
+	protected:
 	public:
 		Message();													//Creates empty(false) object
 		Message(int);
@@ -31,7 +35,12 @@ class Message: public iMessage{
 };
 
 class VoiceMessage: public iMessage{
-
+		VoiceMessage();													//Creates empty(false) object
+		VoiceMessage(int);
+		VoiceMessage(std::string, time_t, int);							//Creates object with id = 0 to add to database
+		VoiceMessage(int, std::string, time_t, int);
+		VoiceMessage(const VoiceMessage&);
+		VoiceMessage operator=(const VoiceMessage&);
 };
 
 #endif
