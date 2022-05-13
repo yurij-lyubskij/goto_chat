bfdef4b44286ff65cea390ad7974a62384f985e1
FackOff25 <kozvladimir@bk.ru>
2022-05-13 10:02:31 +0000

    minor changes

--- internal/ChatRoom/tests/reChatRoom.cpp
+++ internal/ChatRoom/tests/reChatRoom.cpp
@@ -4,33 +4,7 @@
 #include "reChatRoom.h"
 #include "ChatRoom.h"
 #include "../WebSocketServer/tests/reWebSocketServer.h"
-/*
-TEST(ChatTests, adding){
-	ChatRoom chat(1);
-	User usr;
-	usr.Id = 1;
-	usr.Name = "abc";
-	usr.PhoneNumber = "1234567890";
-	chat.addUser(usr);
-	std::vector<User> mems = chat.getMembers();
-	EXPECT_EQ(mems.size(), 1);
-	EXPECT_EQ(mems[0].Id, 1);
-	EXPECT_EQ(mems[0].Name, "abc");
-	EXPECT_EQ(mems[0].PhoneNumber, "1234567890");
-}
 
-TEST(ChatTests, removing){
-	ChatRoom chat(1);
-	User usr;
-	usr.Id = 1;
-	usr.Name = "abc";
-	usr.PhoneNumber = "1234567890";
-	chat.addUser(usr);
-	chat.removeUser(usr);
-	std::vector<User> mems = chat.getMembers();
-	EXPECT_EQ(mems.size(), 0);
-}
-*/
 TEST(ChatTests, reaction){
 	reWebSocketServer serv;
 	ChatRoom chat(1);
--- internal/DBRepo/tests/reDBRepo.cpp
+++ internal/DBRepo/tests/reDBRepo.cpp
@@ -101,6 +101,34 @@ TEST(ChatRepoTests, putAndgetById){
 	EXPECT_EQ(chats[1].getId(), chat2.getId());
 }
 
+/*
+TEST(ChatRepoTests, adding){
+	ChatRoom chat(1);
+	User usr;
+	usr.Id = 1;
+	usr.Name = "abc";
+	usr.PhoneNumber = "1234567890";
+	chat.addUser(usr);
+	std::vector<User> mems = chat.getMembers();
+	EXPECT_EQ(mems.size(), 1);
+	EXPECT_EQ(mems[0].Id, 1);
+	EXPECT_EQ(mems[0].Name, "abc");
+	EXPECT_EQ(mems[0].PhoneNumber, "1234567890");
+}
+
+TEST(ChatTests, removing){
+	ChatRoom chat(1);
+	User usr;
+	usr.Id = 1;
+	usr.Name = "abc";
+	usr.PhoneNumber = "1234567890";
+	chat.addUser(usr);
+	chat.removeUser(usr);
+	std::vector<User> mems = chat.getMembers();
+	EXPECT_EQ(mems.size(), 0);
+}
+*/
+
 //
 //end of ChatRepo Section
 //
