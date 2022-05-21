3a47b63284931354889404ba2d8fb338bfa0197f
FackOff25 <kozvladimir@bk.ru>
2022-05-22 02:37:03 +0300

    First postgresql function

--- internal/DBRepo/DBConnection.cpp
+++ internal/DBRepo/DBConnection.cpp
@@ -2,6 +2,8 @@
 #include <mutex>
 #include <condition_variable>
 
+#include <libpq-fe.h>
+
 #include "DBRepo.h"
 
 
@@ -54,4 +56,48 @@ void DBConnection<Connection>::createPool()
 
 //
 //end of DBConnection Section
+//
+
+//
+//PGConnection Section
+//
+
+PGConnection::PGConnection(){
+    m_connection.reset( 
+        PQsetdbLogin(   m_dbhost.c_str(), 
+                        std::to_string(m_dbport).c_str(), 
+                        nullptr, 
+                        nullptr, 
+                        m_dbname.c_str(), 
+                        m_dbuser.c_str(), 
+                        m_dbpass.c_str()), 
+                    &PQfinish );
+
+    if (PQstatus( m_connection.get() ) != CONNECTION_OK && PQsetnonblocking(m_connection.get(), 1) != 0 ){
+       throw std::runtime_error( PQerrorMessage( m_connection.get() ) );
+    }
+}
+
+std::vector<DBObject> PGConnection::putUsers(std::vector<DBObject> users){
+    //suppose to be                  INSERT INTO users(us_name, us_phone) VALUES(;
+    const std::string baseRequest = "INSERT INTO " + usersTableName + "(" + userNameCol + ", " + userPhoneCol + ") VALUES(";
+    const std::string endRequest = ")  RETURNING " + userIdCol + ";\n";
+    std::string request = "";
+    std::vector<DBObject> ids;
+    //making request
+    for(DBObject object: users)
+        request += baseRequest + "'" + object.attr[1] + "' ," + "'" + object.attr[2] +"'" + endRequest;
+    //sending request
+    PQsendQuery( m_connection.get(), request.c_str() );
+    //getting ids
+    PGresult* res;
+    while ( res = PQgetResult( conn->connection().get()) ) {
+        if (PQresultStatus(res) == PGRES_TUPLES_OK && PQntuples(res_)) {
+            ids.push_back( std::stoi( PQgetvalue (res ,0, 0) ) );
+            PQclear( res );
+        }
+    }
+};
+//
+//end of PGConnection Section
 //
