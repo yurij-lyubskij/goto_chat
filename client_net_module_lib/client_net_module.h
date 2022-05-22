a0a98809e7581b7b1cc616e4bd0d23ac09174bc3
Riniskh <riniskh@mail.ru>
2022-05-22 18:33:47 +0300

    Net

--- client_net_module_lib/client_net_module.h
+++ client_net_module_lib/client_net_module.h
@@ -1,22 +1,53 @@
 // Created by RIskhakov1
 
 #include <string>
+#include <vector>
+#include <thread>
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/noncopyable.hpp>
 
 #ifndef CLIENT_NET_MODULE_H
 #define CLIENT_NET_MODULE_H
 
-class NetModule
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace http = beast::http;
+using tcp = asio::ip::tcp;
+
+class NetModule : public std::enable_shared_from_this<NetModule>
 {
 public:
+
+    [[nodiscard]] static std::shared_ptr<NetModule> Create();
+
+    void Run(const std::string &host, const std::string &port);
+
+    void Send(const std::string&);
+
+    void Disconnect();
+
+private:
     NetModule();
+    void OnResolve(beast::error_code, tcp::resolver::results_type);
+    void OnConnect(beast::error_code, tcp::resolver::results_type::endpoint_type);
+    void OnHandshake(beast::error_code);
+    void Listen();
+    void OnRead(beast::error_code, size_t);
+    void Parse(beast::error_code, size_t);
+    void OnClose(beast::error_code);
 
-    void Run();
+    asio::io_context ioc;
+    tcp::resolver resolver;
+    websocket::stream<beast::tcp_stream> ws;
+    beast::flat_buffer buf;
 
-    void Send();
+    std::string host;
+    std::string port;
 
-    int Poll(std::string);
-
-    int LongPoll(std::string);
+    std::vector<std::thread> threads;
 };
 
 #endif
